8526b6bebec0e6805c84585374f0b6d5b1349e76
lakkuvijay <vijay.lvreddy@gmail.com>
2021-08-11 13:29:16 +0530

    first commit

---
+++ src/test/java/co/in/vijay/ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.in.vijay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
